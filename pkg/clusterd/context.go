19ab7d24be93f930cff1dc581951bcac892235de
caoxianfei1 <18251716983@163.com>
2023-04-15 11:38:08 +0800

    update format progress display log

--- pkg/clusterd/context.go
+++ pkg/clusterd/context.go
@@ -2,10 +2,14 @@ package clusterd
 
 import (
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type Context struct {
+	// The kubernetes config used for this context
+	KubeConfig *rest.Config
+
 	// Clientset is a connection to the core kubernetes API
 	Clientset kubernetes.Interface
 
